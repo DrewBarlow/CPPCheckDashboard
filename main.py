585554ef57b508369edb3ea25d209e7798289c4c
Drew Barlow <drew.barlow@umbc.edu>
2023-01-21 13:02:09 -0500

    basic setup

---
+++ main.py
@@ -0,0 +1,13 @@
+import sa_coverage
+from argparse import ArgumentParser, Namespace
+
+def main() -> None:
+    parser: ArgumentParser = ArgumentParser()
+    # add args
+    args: Namespace = parser.parse_args()
+
+    return
+
+if __name__ == "__main__":
+    main()
+
