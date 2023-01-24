f59415106a2b7f275dd81aa4c6eb8a03f05f449e
Drew Barlow <drew.barlow@umbc.edu>
2023-01-23 23:30:45 -0500

    updated typing documentation

--- sa_coverage/analyzer.py
+++ sa_coverage/analyzer.py
@@ -17,7 +17,7 @@ def analyze(path_to_analyzer: str, cmd_args: str, /, *, to_stdout: bool=False) -
 
     Parameters:
         path_to_analyzer (str): The analyzer in question.
-        cmd_args (List[str]): The arguments to be passed to the analyzer.
+        cmd_args (str): The arguments to be passed to the analyzer.
         to_stdout (bool, optional): Generates a .html file if False, otherwise prints to stdout.
 
     Returns:
