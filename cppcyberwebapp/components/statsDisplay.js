617f2416238f73a84d9dc3de10999b71f00f1b2c
Rich4ever11 <67285654+Rich4ever11@users.noreply.github.com>
2023-04-25 14:11:12 -0400

    Add files via upload

---
+++ cppcyberwebapp/components/statsDisplay.js
@@ -0,0 +1,38 @@
+export default function statDisplay({
+  linesOfCode,
+  errorsFound,
+  nonLinesOfCode,
+}) {
+  return (
+    <div>
+      <h1 className="py-4 text-5xl font-bold text-white">{"Scan Statistic"}</h1>
+      <div className="stats stats-vertical lg:stats-vertical shadow bg-neutral-focus">
+        <div className="stat">
+          <div className="stat-title">LOC Total</div>
+          <div className="stat-value">{linesOfCode || 0}</div>
+          <div className="stat-desc">Lines Of Code Total</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-title">Error's Found</div>
+          <div className="stat-value">{errorsFound || 0}</div>
+          <div className="stat-desc">Error's Found by Analyzer</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-title">LOC Not Analyzed</div>
+          <div className="stat-value">{nonLinesOfCode || 0}</div>
+          <div className="stat-desc">Lines Of Code Not Analyzed</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-title">NOC Preformed</div>
+          <div className="stat-value">{27}</div>
+          <div className="stat-desc">
+            Number of Checks The Program Was Preformed Against
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
