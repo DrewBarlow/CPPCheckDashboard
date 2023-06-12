068e5bebe5a6c7de53d06acde88074c1e45443fb
Suraj Pammi <48070125+SurajPammi@users.noreply.github.com>
2023-06-12 16:53:48 -0400

    Scripts for integration

---
+++ make_dependencies.sh
@@ -0,0 +1,5 @@
+cd sa_coverage/cppcheck
+make
+cd ..
+
+pip3 install -r requirements.txt
---
+++ run_script.sh
@@ -0,0 +1,5 @@
+python3 expanded_dictionary.py
+cd cppcyberwebapp
+python3 program.py
+open out/index.html
+cd ../..
